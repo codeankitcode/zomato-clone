bc2e658e679f051ef29866c84cfc775e4692f706
Ankit Roy <ankitdasout@gmail.com>
2023-09-19 17:00:30 +0530

    Update TabOptions.jsx

--- src/components/TabOptions.jsx
+++ src/components/TabOptions.jsx
@@ -8,14 +8,14 @@ export default function TabOptions(){
                 <img src="https://b.zmtcdn.com/data/o2_assets/c0bb85d3a6347b2ec070a8db694588261616149578.png" alt="" />   
                 <p>Delivery</p>
             </NavLink>
-            <div>
-                <img src="" alt="" />
+            <NavLink>
+                <img src="https://b.zmtcdn.com/data/o2_assets/78d25215ff4c1299578ed36eefd5f39d1616149985.png" alt="" />
                 <p>Dining Out</p>
-            </div>
-            <div>
-                <img src="" alt="" />
+            </NavLink>
+            <NavLink>
+                <img src="https://b.zmtcdn.com/data/o2_assets/01040767e4943c398e38e3592bb1ba8a1616150142.png" alt="" />
                 <p>Nightlife</p>
-            </div>
+            </NavLink>
         </div>
     )
 }
